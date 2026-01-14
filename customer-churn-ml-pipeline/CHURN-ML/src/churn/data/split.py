27f2bb67f292ce748727f9a2634d447fdbce55d4
alchem12345 <saivikhyath21@gmail.com>
2026-01-14 12:37:29 +0530

    .

--- customer-churn-ml-pipeline/CHURN-ML/src/churn/data/split.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-def split_data(df , target_col , test_size , random_state):
-    X = df.drop( columns = [target_col])
-    y = df[target_col].map({'Yes':1,
-                            'No' : 0})
-    
-    return train_test_split(X,y,test_size=test_size,random_state=random_state)
--- customer-churn-ml-pipeline/CHURN-ML/src/churn/models/train.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-def train(pipeline ,X_train , Y_train , model_path):
-    pipeline.fit(X_train,Y_train)
-    joblib.dump(pipeline,model_path)
-
-    return pipeline
-
-    
